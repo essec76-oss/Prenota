d38b9e4643bed5716799a0328aa2fdd62493edbe
essec76-oss <esse.c76@gmail.com>
2026-09-09 17:21:42 +0200

    Update Supabase service key environment variable

--- api/2fa-setup.js
+++ api/2fa-setup.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const QRCode = require('qrcode');
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 
 function generateBase32Secret(length = 32) {
